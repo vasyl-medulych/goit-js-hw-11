1e3d7f085c83979265276f5476591381c2b9ad2c
Vasyl Linkin <linkinmvv@gmail.com>
2025-08-09 11:34:07 +0300

    init

---
+++ src/js/render-functions.js
@@ -0,0 +1,10 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+export function createGallery(images) {}
+
+export function clearGallery() {}
+
+export function showLoader() {}
+
+export function hideLoader() {}
