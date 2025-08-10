63848fcd1ec931c63125cfe595acfd8b97ef04e4
Vasyl Linkin <linkinmvv@gmail.com>
2025-08-10 15:16:21 +0300

    task copmleted

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,10 +1,59 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+const ulElem = document.querySelector('.gallery');
+const gallery = new SimpleLightbox('.gallery-item a', {
+  captionsData: 'alt', // Вказуємо, що підписи братимуться з атрибута 'alt' зображення
+  captionDelay: 250, // Затримка появи підпису
+});
 
-export function createGallery(images) {}
+export function createGallery(images) {
+  const markup = templatesImage(images);
+  ulElem.insertAdjacentHTML('afterbegin', markup);
 
-export function clearGallery() {}
+  gallery.refresh();
+}
 
-export function showLoader() {}
+export function clearGallery() {
+  ulElem.innerHTML = ``;
+}
 
-export function hideLoader() {}
+export function showLoader() {
+  const loader = `<span class="loader"></span>`;
+  ulElem.insertAdjacentHTML('afterbegin', loader);
+}
+
+export function hideLoader() {
+  const loader = ``;
+  ulElem.innerHTML = loader;
+}
+
+function templateImage(img) {
+  const {
+    webformatURL,
+    largeImageURL,
+    tags,
+    likes,
+    views,
+    comments,
+    downloads,
+  } = img;
+  return `<li class="gallery-item">
+  <a class="gallery-link" href="${largeImageURL}">
+    <img
+      class="gallery-image"
+      src="${webformatURL}"
+      alt="${tags}"
+    />
+  </a><div class="stats">
+  <p class="img-rates">Likes<span>${likes}</span></p>
+  <p class="img-rates">Views<span>${views}</span></p>
+  <p class="img-rates">Comments<span>${comments}</span></p>
+  <p class="img-rates">Downloads<span>${downloads}</span></p></div>
+</li>
+`;
+}
+
+function templatesImage(imgs) {
+  console.log('imgs = ', imgs);
+  return imgs.map(templateImage).join('\n');
+}
